a931356ad5d393f91c5afd2dc903b69f8d9e96bb
Ghulam Subbhani <gsmalik030@gmail.com>
2023-03-15 14:20:02 +0200

    _Fixed bugs

--- src/--test--/Home.test.js
+++ src/--test--/Home.test.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
-import Home from '../components/home';
+import Home from '../components/Home';
 
 it('renders correctly', () => {
   const home = renderer
